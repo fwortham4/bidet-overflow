9cf4acafb46288ea2e0b7b87a1c3c0f78d699d57
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-08-31 15:18:42 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :votes
   has_many :questions
+
+  validates_presence_of :user_name, :email, :hash_password
+  validates_uniqueness_of :email
+  validates :password_length, :valid_email
+
+  def password_length
+    if @raw_password.length < 6
+      errors.add(:password, "must be 6 at least 6 characters!")
+    end
+  end 
+
+  def valid_email
+    if !self.email.include? "@"
+      errors.add(:email, "must contain @ symbol to be an email!")
+    end
+  end
 end
