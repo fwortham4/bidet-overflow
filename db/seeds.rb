6bef9dcbc81768fbfefaf466baea2a7d02bad10a
[01;31m[K"name"[m[K: "ThuyNT13 <[01;31m[K"email"[m[K: null>
2016-09-01 15:28:35 -0500

    add test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@ User.create(user_name: "billybob", email: "billy_bob@gmail.com", password: "pass
 Comment.create(content: "gummy worms are fucking stupid", user_id: 2, commentable_id: 1, commentable_type: "Question")
 
 Answer.create(content: "I think your love for gummy worms is legitamate", question_id: 1, user_id: 2)
+
+# User.create(user_name: 'thuy', email: 'thuy@email.com', password: '12345pass')
